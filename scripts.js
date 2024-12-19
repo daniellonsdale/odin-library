0e6db3892716c6fd9584ec576f0e9671c18c57ed
daniellonsdale <danielllonsdale@gmail.com>
2024-12-20 00:23:19 +0900

    Test and fix displayBooks

--- scripts.js
+++ scripts.js
@@ -14,45 +14,53 @@ function addBookToLibrary(book) {
 
 function displayBooks(){
     for (const i in myLibrary) {
-        
+        createCard(myLibrary[i]);
     }
 }
 
 function createCard(book){
     const bookCard = document.createElement('div');
-    bookCard.classList.add('.book-card');
+    bookCard.classList.add('book-card');
 
     const title = document.createElement('h1');
-    title.classList.add('.title');
+    title.classList.add('title');
     title.textContent = book.title;
     bookCard.appendChild(title);
 
     const removeButton = document.createElement('button');
-    removeButton.classList.add('.remove-button');
+    removeButton.classList.add('remove-button');
     removeButton.setAttribute("type", "button");
     removeButton.textContent = "X";
     bookCard.appendChild(removeButton);
 
     const author = document.createElement('h2');
-    author.classList.add('.author');
+    author.classList.add('author');
     author.textContent = book.author;
     bookCard.appendChild(author);
 
     const pages = document.createElement('h2');
-    pages.classList.add('.pages');
+    pages.classList.add('pages');
     pages.textContent = book.numPages;
     bookCard.appendChild(pages);
 
     const read = document.createElement('h2');
-    read.classList.add('.read');
+    read.classList.add('read-boolean');
     if(book.read === true){
         read.textContent = "READ";
-        read.classList.add('.read');
+        read.classList.add('read');
     }else{
         read.textContent = "NOT READ";
-        read.classList.add('.not-read');
+        read.classList.add('not-read');
     }
     bookCard.appendChild(read);
 
     bookContainer.appendChild(bookCard);
 }
+
+const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, false);
+const nineEigthFour = new Book('1984', 'George Orwell', 316, true);
+
+addBookToLibrary(theHobbit);
+addBookToLibrary(nineEigthFour);
+
+displayBooks();
